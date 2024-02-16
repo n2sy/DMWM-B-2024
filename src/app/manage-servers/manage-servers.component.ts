1ea87f738e8af9593d225a625b8709ffd986245b
n2sy <nidhal.jelassi@fsegt.utm.tn>
2024-02-16 14:47:39 +0100

    exercice ngclass

---
+++ src/app/manage-servers/manage-servers.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-servers',
+  templateUrl: './manage-servers.component.html',
+  styleUrl: './manage-servers.component.css',
+})
+export class ManageServersComponent {
+  tabServers = [
+    {
+      name: 'Development Server',
+      type: 'small',
+      date_d: new Date(2023, 3, 5),
+      statut: 'critical',
+    },
+    {
+      name: 'Testing Development Server',
+      type: 'large',
+      date_d: new Date(2022, 5, 3),
+      statut: 'stable',
+    },
+    {
+      name: 'Production Server',
+      type: 'small',
+      date_d: new Date(2023, 3, 5),
+      statut: 'offline',
+    },
+    {
+      name: 'Nidhal Server',
+      type: 'medium',
+      date_d: new Date(2023, 3, 5),
+      statut: 'stable',
+    },
+  ];
+
+  affecterClass(st) {
+    return {
+      'list-group-item-success': st == 'stable', // ? true : false,
+      'list-group-item-danger': st == 'critical',
+      'list-group-item-warning': st == 'offline',
+    };
+  }
+}
